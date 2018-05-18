df285a026bd3ab98c4f01dc38aa0dabc67858098
Kyle Holzinger <kylelholzinger@gmail.com>
2018-05-18 18:51:15 +0000

    [KH] make example more realistic

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import compose from '@kyleholzinger/react-compose'
 import './App.css'
 import Dump from './Dump'
 
-class ComponentA extends Component {
+class MousePosition extends Component {
   render() {
-    return <Fragment>{this.props.children({ someValue: 1 })}</Fragment>
+    return <Fragment>{this.props.children({ x: 300, y: 427 })}</Fragment>
   }
 }
 
-class ComponentB extends Component {
+class Subscription extends Component {
   render() {
-    return <Fragment>{this.props.children({ someOtherValue: 2 })}</Fragment>
+    return <Fragment>{this.props.children({ data: ["note 1", "note 2"] })}</Fragment>
   }
 }
 
-class ComponentC extends Component {
+class Validate extends Component {
   render() {
-    return <Fragment>{this.props.children({ someThirdValue: 3 })}</Fragment>
+    return <Fragment>{this.props.children({ predicate: x => (x < 10) })}</Fragment>
   }
 }
 
 const ComposedComponent = compose([
-  { component: ComponentA, props: { testPropA: 1 } },
-  { component: ComponentB, props: { testPropB: 2 } },
-  { component: ComponentC, props: { testPropC: 3 } }
+  { component: MousePosition, props: { relative: true } },
+  { component: Subscription, props: { url: '/api/notes' } },
+  { component: Validate  }
 ])
 
 class App extends Component {
